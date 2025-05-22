3bcc731cb79058282028e8fae5be0277aafdcf5f
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-05-22 18:14:19 +0200

    fix: Adjust padding and layout in Hero

--- src/components/page-components/Hero/Hero.tsx
+++ src/components/page-components/Hero/Hero.tsx
@@ -6,17 +6,20 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section id="home" className="relative z-10 overflow-hidden bg-gradient-to-b from-[#27665D] to-[#3A3A3A] pt-[160px] pb-24 md:pt-[160px] md:pb-[32px] xl:pt-[160px] xl:pb-[60px] 2xl:pt-[180px] 2xl:pb-[80px]">
+    <section
+      id="home"
+      className="relative z-10 overflow-hidden bg-gradient-to-b from-[#27665D] to-[#3A3A3A] pt-[130px] pb-24 md:pt-[120px] md:pb-[32px] xl:pt-[140px] xl:pb-[60px] 2xl:pt-[160px] 2xl:pb-[80px]"
+    >
       <div className="flex flex-col container max-w-7xl mx-auto gap-8">
-        <div className="flex flex-col justify-center items-center container max-w-6xl mx-auto gap-6">
+        <div className="flex flex-col justify-center items-center container max-w-6xl mx-auto gap-7">
           <Badge className="bg-blue-50 text-black flex gap-1 rounded-full px-4 py-1.5">
             <ShieldCheck className="w-6 h-6 text-blue-400" />
             SOC 2 Compliant
           </Badge>
-          <h1 className="font-bold text-5xl text-primary-foreground max-w-2xl text-center mx-auto">
+          <h1 className="font-bold text-5xl text-primary-foreground max-w-xl text-center mx-auto leading-snug">
             The Smarter Way to Manage Procurement
           </h1>
-          <p className="font-medium text-primary-foreground max-w-2xl mx-auto text-center">
+          <p className="text-primary-foreground max-w-3xl mx-auto text-center text-lg">
             EP365 simplifies your procurement process with powerful automation,
             real-time insights, and AI-driven supplier management — all in one
             user-friendly platform.
