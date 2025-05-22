127126eb480d6ed47a115bdb6787e8b46ae48e2e
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-05-22 18:12:58 +0200

    feat: Add Description component

---
+++ src/components/page-components/Banner/Description.tsx
@@ -0,0 +1,37 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import Image from "next/image";
+
+const Description = () => {
+  return (
+    <section id="description" className="relative z-10 pt-16 md:pt-20 lg:pt-24">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: false }}
+        className="max-w-5xl mx-auto rounded-2xl flex justify-between bg-gradient-to-tr from-white via-white to-primary/30"
+      >
+        <div className="flex flex-col gap-4 p-16">
+          <h2 className="font-medium text-md uppercase">
+            Ai designed for procurement
+          </h2>
+          
+          <h1 className="font-semibold text-3xl max-w-sm">
+            Your AI Assistant for Purchasing
+          </h1>
+          <p className="max-w-sm text-sm">
+            Let EP365 handle the heavy lifting. Our AI-powered system
+            automatically collects supplier quotes, compares pricing, and
+            identifies the best deals — so you can move faster and save more.
+          </p>
+        </div>
+        <div className="py-4">
+          <Image src="/images/banner/Procurify-AI-designed-for-procurement.webp.svg" className="h-max" width={400} height={400} alt="Banner Image"/>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Description;
--- src/components/page-components/Banner/bottom-banner.tsx
+++ src/components/page-components/Banner/bottom-banner.tsx
@@ -9,14 +9,14 @@ const Banner = () => {
       initial="hidden"
       whileInView="visible"
       viewport={{ once: false }}
-      className="max-w-5xl mx-auto rounded-2xl bg-neutral-800 p-12 flex justify-between"
+      className="max-w-5xl my-12 mx-auto rounded-2xl bg-neutral-800 p-16 flex justify-between"
     >
       <div className="text-white flex flex-col gap-1">
         <h1 className="text-3xl font-semibold">See what EP365 has for you!</h1>
         <p>Start your spend visibilty now</p>
       </div>
       <div>
-        <Button className="bg-primary/50 p-4">Get my demo</Button>
+        <Button className="bg-primary p-6 text-md">Get my demo</Button>
       </div>
     </motion.div>
   );
