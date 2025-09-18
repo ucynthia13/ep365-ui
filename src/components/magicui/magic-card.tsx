ce1a19b7812a7085dbc9e8249a24fd876c7b9a34
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-09-18 21:50:01 +0200

    update background for magic-card

--- src/components/magicui/magic-card.tsx
+++ src/components/magicui/magic-card.tsx
@@ -81,7 +81,7 @@ export function MagicCard({
       className={cn("group relative rounded-[inherit]", className)}
     >
       <motion.div
-        className="pointer-events-none absolute inset-0 rounded-[inherit] bg-border duration-300 group-hover:opacity-100"
+        className="pointer-events-none absolute inset-0 rounded-[inherit] duration-300 group-hover:opacity-100"
         style={{
           background: useMotionTemplate`
           radial-gradient(${gradientSize}px circle at ${mouseX}px ${mouseY}px,
