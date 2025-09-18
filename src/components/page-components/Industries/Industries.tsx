87e8e3debc1c4b726c1443919008b3ecd74c3eec
Cynthia Umwali <cynthiaumwali3@gmail.com>
2025-09-18 21:51:51 +0200

    update Industries component

--- src/components/page-components/Industries/Industries.tsx
+++ src/components/page-components/Industries/Industries.tsx
@@ -6,24 +6,26 @@ import FeaturesSection from './FeaturesSection';
 import IndustriesSection from './IndustriesSection';
 import Section from '../common/Section';
 import SectionTitle from '../common/SectionTitle';
+import { Badge } from '@/components/ui/badge';
 
 const Industries = () => {
   const [activeSection, setActiveSection] = useState<'features' | 'industries'>('features');
 
   return (
-    <Section id="industries"
+    <Section id="industries" className="border-divide border-x pb-20 md:pb-24"
     >
       <div className="flex flex-col" id="solutions">
         <div className="flex flex-col justify-center items-center gap-4">
-          <div className="max-w-3xl mx-auto text-center">
-            <SectionTitle title="All-in-One Procurement Platform" className="mb-2" />
-            <p className="text-center text-base mb-4">
+          <div className="max-w-3xl mx-auto flex flex-col justify-center items-center gap-4 text-center">
+            <Badge variant="outline" className="text-xs text-primary border-none font-medium">Solutions & Industries</Badge>
+            <SectionTitle title="All-in-One Procurement Platform" />
+            <p className="text-base mb-4 text-muted-foreground">
               One platform to streamline your entire procurement lifecycle, globally and efficiently.
             </p>
           </div>
           <div className="flex gap-4">
             <Button
-              className={`px-6 py-5 rounded-full cursor-pointer ${
+              className={`px-6 py-5 cursor-pointer ${
                 activeSection === 'features'
                   ? 'bg-black text-white hover:bg-gray-900'
                   : 'bg-white text-black border border-gray-400'
@@ -33,7 +35,7 @@ const Industries = () => {
               Solutions
             </Button>
             <Button
-              className={`px-6 py-5 rounded-full cursor-pointer ${
+              className={`px-6 py-5 cursor-pointer ${
                 activeSection === 'industries'
                   ? 'bg-black text-white hover:bg-gray-900'
                   : 'bg-white text-black border border-gray-400'
